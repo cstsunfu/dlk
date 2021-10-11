df9287dc1dfb02cbbc7704b011a1312eedee29cf
cstsunfu <cstsunfu@gmail.com>
2021-10-11 17:13:14 +0800

    move tokenizer processors to util

---
+++ dlkit/processors/util/tokenizer_processor.py
@@ -0,0 +1,111 @@
+from tokenizers.processors import TemplateProcessing
+from tokenizers.normalizers import Lowercase, NFD, NFC, StripAccents, Strip
+from tokenizers.pre_tokenizers import Whitespace, ByteLevel
+
+
+class TokenizerPostprocessorFactory(object):
+    """docstring for TokenizerPostprocessorFactory"""
+        
+    @property
+    def bert(self):
+        """TODO: Docstring for bert.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        return TemplateProcessing(
+            single="[CLS] $A [SEP]",
+            pair="[CLS] $A [SEP] $B:1 [SEP]:1",
+            special_tokens=[
+                ("[CLS]", 1),
+                ("[SEP]", 2),
+            ],
+        )
+
+
+    def get(self, name):
+        """TODO: Docstring for get.
+        """
+        return self.__getattribute__(name)
+
+
+
+class PreTokenizerFactory(object):
+    """docstring for PreTokenizerFactory"""
+        
+    @property
+    def bytelevel(self):
+        """TODO: Docstring for bytelevel.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        return ByteLevel
+
+    @property
+    def whitespace(self):
+        """TODO: Docstring for bytelevel.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        return Whitespace
+
+    def get(self, name):
+        """TODO: Docstring for get.
+
+        :name: TODO
+        :returns: TODO
+
+        """
+        return self.__getattribute__(name)
+
+
+class TokenizerNormalizerFactory(object):
+    """docstring for TokenizerNormalizerFactory"""
+        
+    @property
+    def lowercase(self):
+        """TODO: Docstring for lowercase.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        return Lowercase
+
+    @property
+    def nfd(self):
+        """TODO: Docstring for nfd.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        return NFD
+
+    @property
+    def nfc(self):
+        """TODO: Docstring for nfc.
+        """
+        return NFC
+
+    @property
+    def strip_accents(self):
+        """
+        """
+        return StripAccents
+
+    @property
+    def strip(self):
+        """
+        """
+        return Strip
+
+    def get(self, name):
+        """TODO: Docstring for get.
+        """
+        return self.__getattribute__(name)
