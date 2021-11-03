d9529d720cde69a459e8a39d35ba5ea21b4279da
cstsunfu <cstsunfu@gmail.com>
2021-11-03 19:01:30 +0800

    add BaseModule Imp

--- tests/trace/dict_trace.py
+++ tests/trace/dict_trace.py
@@ -1,19 +1,126 @@
 import torch
 import torch.nn as nn
 from collections import namedtuple
-from typing import Dict
+from typing import Dict, List
 import numpy as np
+import abc
 
 
-class Module(nn.Module):
+class ModuleOutputRenameMixin:
+    """Just rename the output key name by config to adapt the input field of downstream module."""
+    def output_rename(self, result: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        return {self.config['output_map'].get(key, key): value for key, value in result.items()}
+        
+
+class IModuleIO(metaclass=abc.ABCMeta):
+    """docstring for IModuleIO"""
+
+    @abc.abstractmethod
+    def provide_keys(self)->List[str]:
+        """TODO: Docstring for provide_keys.
+        :returns: TODO
+        """
+        pass
+
+    @abc.abstractmethod
+    def check_keys_are_provided(self, provide: List[str])->bool:
+        """TODO: Docstring for check_keys_are_provided.
+        :returns: TODO
+        """
+        pass
+
+    def check_module_chain_passed(self, module_list: List["BaseModule"]):
+        """check the interfaces of the list of modules are alignd or not.
+
+        :List[nn.Module]: all modules in this chain
+        :returns: None, if check not passed, raise a ValueError
+        """
+        assert len(module_list) > 1
+        result = True
+        for i in range(len(module_list)-1):
+            result = result and module_list[i+1].check_keys_are_provided(module_list[i].provide_keys())
+            if not result:
+                raise ValueError(f'The module "{module_list[i+1]._name}" is required "{", ".join(module_list[i+1].provide_keys())}", \
+                    but the module "{module_list[i]._name}" provide "{", ".join(module_list[i].provide_keys())}"! ')
+
+
+class IModuleStep(metaclass=abc.ABCMeta):
+    """docstring for ModuleStepMixin"""
+
+
+    @abc.abstractmethod
+    def predict_step(self, inputs: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        """predict
+        :inputs: Dict[str: torch.Tensor], one mini-batch inputs
+        :returns: Dict[str: torch.Tensor], one mini-batch outputs
+        """
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def training_step(self, inputs: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        """training
+        :inputs: Dict[str: torch.Tensor], one mini-batch inputs
+        :returns: Dict[str: torch.Tensor], one mini-batch outputs
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def valid_step(self, inputs: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        """valid
+        :inputs: Dict[str: torch.Tensor], one mini-batch inputs
+        :returns: Dict[str: torch.Tensor], one mini-batch outputs
+        """
+        raise NotImplementedError
+
+class BaseModule(nn.Module, ModuleOutputRenameMixin, IModuleIO, IModuleStep):
+    """docstring for BaseModule"""
+
+    def forward(self, inputs: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        """predict forward
+        """
+        return self.predict_step(inputs)
+        
+
+class Module(BaseModule):
     """docstring for Module"""
     def __init__(self):
         super(Module, self).__init__()
         self.linear = nn.Linear(3, 4)
         self.linear2 = nn.Linear(4, 3)
+        self.config = {}
+        self.config['output_map'] = {"inp":"map_inp"}
+        # self._name_map = {"inp":"map_inp"}
+        self.provide = {''}
+        self.required = {''}
         
+    def training_step(self, inputs: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        """training
+        :inputs: Dict[str: torch.Tensor], one mini-batch inputs
+        :returns: Dict[str: torch.Tensor], one mini-batch outputs
+        """
+        raise NotImplementedError
 
-    def forward(self, args: Dict[str, torch.Tensor]):
+    def valid_step(self, inputs: Dict[str, torch.Tensor])->Dict[str, torch.Tensor]:
+        """valid
+        :inputs: Dict[str: torch.Tensor], one mini-batch inputs
+        :returns: Dict[str: torch.Tensor], one mini-batch outputs
+        """
+        raise NotImplementedError
+
+    def provide_keys(self)->List[str]:
+        """TODO: Docstring for provide_keys.
+        :returns: TODO
+        """
+        pass
+        # raise NotImplementedError
+
+    def check_key_are_provided(self, provide: List[str])->bool:
+        """TODO: Docstring for check_key_are_provided.
+        :returns: TODO
+        """
+        pass
+
+    def predict_step(self, args: Dict[str, torch.Tensor]):
         """TODO: Docstring for forward.
 
         :**args: TODO
@@ -31,7 +138,9 @@ class Module(nn.Module):
         x = self.linear2(x)
         # print(x)
         # nt = namedtuple("nt", ['inp', 'out'], defaults=[None]*2)
-        return {"inp": inp, "out": out, "x": x}
+        result = {"inp": inp, "out": out, "x": x}
+        # result = {self.name_map.get(key, key): value for key, value in result.items()}
+        return self.output_rename(result)
         # return nt(out=out)
 
 
@@ -43,11 +152,12 @@ model = Module()
 # print(d)
 # script = torch.jit.trace(model, d, strict=False)
 model.eval()
+# print(model(d))
 script = torch.jit.script(model)
-script.train()
+# script.train()
 print(script(d))
-script.eval()
-print(script(d))
+# script.eval()
+# print(script(d))
 # # a = torch.randn(5, 3)
 # nt = namedtuple("nt", ['inp', 'out'], defaults=None)
 # out = nt._make(script(d))
