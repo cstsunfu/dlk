c9e78babbad804a6820a3e0efc440d81f7517983
cstsunfu <cstsunfu@gmail.com>
2022-11-20 18:39:17 +0800

    chore: fix random embedding shape mismatch

--- dlk/core/layers/embeddings/random.py
+++ dlk/core/layers/embeddings/random.py
@@ -66,8 +66,8 @@ class RandomEmbedding(SimpleModule):
         self._required_keys = {'input_ids'} # required by this module
         self.config = config
         self.dropout = nn.Dropout(float(self.config.dropout))
-        normal =  torch.distributions.Normal(torch.tensor([0.0]), torch.tensor([2.0/self.config.embedding_dim]))
-        self.embedding = nn.Embedding.from_pretrained(normal.sample((self.config.vocab_size, self.config.embedding_dim)), padding_idx=self.config.padding_idx)
+        normal = torch.distributions.Normal(torch.tensor([0.0]), torch.tensor([2.0/self.config.embedding_dim]))
+        self.embedding = nn.Embedding.from_pretrained(normal.sample((self.config.vocab_size, self.config.embedding_dim)).squeeze_(-1), padding_idx=self.config.padding_idx)
 
     def init_weight(self, method: Callable):
         """init the weight of submodules by 'method'
