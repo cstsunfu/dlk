e675ca7c0cb9bbc1081cad96e2075f193d01d034
cstsunfu <cstsunfu@gmail.com>
2025-02-05 01:00:51 +0800

    dev: make dlk to subpackages for export/predict/train

--- dlk/callback/weight_average.py
+++ dlk/callback/weight_average.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the Apache license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from intc import (
     MISSING,
